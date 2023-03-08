a424bb729bc5fd8fc9a34a50cf4549232eabbf60
ChanHHOO <hpyho33@kookmin.ac.kr>
2023-03-08 20:40:43 +0900

    feature : send email module

---
+++ ai_model/sendEmail.py
@@ -0,0 +1,24 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(subject, message, from_email, to_email, password):
+    # Create message container
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    # Attach message to email
+    msg.attach(MIMEText(message, 'plain'))
+
+    # Create SMTP server object and send email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_email, password)
+    text = msg.as_string()
+    server.sendmail(from_email, to_email, text)
+    server.quit()
+
+# Example usage
+send_email("Test email", "Hello, this is a test email!", "hpyho112@gmail.com", "hpyho33@kookmin.ac.kr", "rvhhkgmicajzelxv")
