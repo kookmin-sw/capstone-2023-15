697e5a00f6be8854a4b9930112ff2825ab45860d
ChanHHOO <hpyho33@kookmin.ac.kr>
2023-03-12 21:47:40 +0900

    featrue : 주석달기

--- ai_model/ImageConvert.py
+++ ai_model/ImageConvert.py
@@ -4,11 +4,21 @@ import os
 
 root_path = "../statics/convert_image"
 
+# get file list
 avif_file = os.listdir(root_path + "/avif")
 
 for file in avif_file:
-    file_name = file.split(".")[0]
-    old_image_path = root_path + "/avif/{}.avif".format(file_name)
-    with Image.open(old_image_path) as im:
-        new_image_path = root_path + "/png/{}.png".format(file_name)
-        im.save(new_image_path)
+
+    # get file name without type
+    file_name, type = file.split(".")
+
+    if type == "avif":
+        old_image_path = root_path + "/avif/{}.avif".format(file_name)
+
+        # open avif file
+        with Image.open(old_image_path) as png_image:
+            new_image_path = root_path + "/png/{}.png".format(file_name)
+            # save png file
+            png_image.save(new_image_path)
+    else:
+        print("{} is not avif type. only allow avif type.".format(file))
