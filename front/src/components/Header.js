592df3199ec892631a61e80d5ec6e59e46b7b47a
Shimhyerin <nier8702@kookmin.ac.kr>
2023-03-26 02:05:28 +0900

    feature : header 구현

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,31 +1,97 @@
 import React from "react";
 import styled from 'styled-components';
-import Logo from '../statics/images/logo-text.png'
+import LogoImg from '../statics/images/logo-text.png'
 
 function Header() {
   return (
-    <HeaderDiv>
-        <LogoImg src={Logo} />
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    </HeaderDiv>
+    <HeaderContainer>
+        <Logo>
+            <NavLink href="/">
+                <LogoImgSection src={LogoImg} />
+            </NavLink>
+        </Logo>
+        <Nav>
+            <NavList>
+                <NavItem>
+                    <NavLink href="/">INTRODUCTION</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/cases">CASE</NavLink>
+                </NavItem>
+                <NavItem>
+                    <ResultBtn>Check the result</ResultBtn>
+                </NavItem>
+            </NavList>
+        </Nav>
+    </HeaderContainer>
   );
 }
 
-const HeaderDiv = styled.div`
+const HeaderContainer = styled.div`
+    // min-width: 1280px;
+    max-height: 5em;
 	font-family: AkiraExpanded;
-
+    font-size: 1em;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `
 
-const LogoImg = styled.img`
-    width: 1em;
-    // height: 1em;
+const Logo = styled.div`
+    width: 25%;
+    margin-right: 5%;
+`
 
+const LogoImgSection = styled.img`
+    width: 100%;
+`
+const Nav = styled.nav`
+    // background: orange;
+    width: 70%;
+
+
+`
+const NavList = styled.ul`
+    display: flex;
+    text-align: center;
+    align-items: center;
+    font-size: 1em;
+    margin-right: 3%;
+    
+`
+
+const NavItem = styled.li`
+    width: 100%;
+    list-style-type: none;
+`
+
+const NavLink = styled.a`
+    text-decoration: none;
+    color: black;
+    &:hover, &:visited{
+        text-decoration: none;
+    };
+    
+`
+
+const ResultBtn = styled.button`
+    padding: 3% 10% 3% 10%;
+    margin: 1%;
+    font-size: 1em;
+    font-family: AkiraExpanded; 
+    border-radius: 20px;
+    border: 2.5px solid black;
+    background-color: white;
+    white-space : nowrap;
+
+    &:hover{
+        background: linear-gradient(to right, #4285ec, #0bbafb);
+        color: white;
+        border: none;
+
+    }
 `
 
 export default Header;
+
