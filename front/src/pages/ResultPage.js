716fee10525dbf170281dcb426b3c53ccef83fbb
ChanHHOO <hpyho33@kookmin.ac.kr>
2023-04-13 03:16:10 +0900

    feature : get results in dynamodb

--- front/src/pages/ResultPage.js
+++ front/src/pages/ResultPage.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import RequestDoneComponent from '../components/ResultComponents/RequestDoneComponent';
 import RequestUndoneComponent from '../components/ResultComponents/RequestUndoneComponent';
+import * as AWS from 'aws-sdk'
 
 import bgGreen from '../statics/images/bg-green.png'
 import bgRed from '../statics/images/bg-red.png'
@@ -23,6 +24,44 @@ const sampleJson = {
 }
 
 const ResultPage = () => {
+	const {
+		REACT_APP_REGION,
+		REACT_APP_ACCESSKEY_ID,
+		REACT_APP_SECRETACCESSKEY,
+	} = process.env;
+
+	var results;
+
+	AWS.config.update({
+		region: REACT_APP_REGION,
+		accessKeyId: REACT_APP_ACCESSKEY_ID,
+		secretAccessKey: REACT_APP_SECRETACCESSKEY,
+	});
+
+	const dynamoDB = new AWS.DynamoDB.DocumentClient();
+	
+	// get
+	try {
+		dynamoDB.query({
+			TableName: "TF_database",
+			KeyConditionExpression: '#dt = :dataCondition',
+			ExpressionAttributeNames: {
+				"#dt": 'data_type'
+			},
+			ExpressionAttributeValues: {
+				':dataCondition': 'response',
+			}
+		}, (err, data) => {
+			if(err)
+				console.error(err);
+			else{
+				results = data.Items;
+			}
+		});
+	} catch {
+		console.error("Fail to get results.. please try again after one minute");
+	}
+
 	return (
 		<ResultPageRoot>
 			<PageTitle>RESULTs</PageTitle>
@@ -56,3 +95,33 @@ const PageTitle = styled.div`
 
 
 export default ResultPage;
+
+// {
+//   "data_type": {
+//     "S": "response"
+//   },
+//   "timestamp": {
+//     "N": "11111111111"
+//   },
+//   "id": {
+//     "S": "0xaEwarso12392samdl"
+//   },
+//   "malicious_images": {
+//     "S": "[{platform:opensea, info:ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/51669552594437927291657533851464109096593131995911303099186686504615012204545}, {platform:opensea, info:ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/51669552594437927291657533851464109096593131995911303099186686505714523832321]"
+//   },
+//   "predict_result": {
+//     "B": "true"
+//   },
+//   "request_id": {
+//     "S": "0x123wsda"
+//   },
+//   "target_images_cnt": {
+//     "N": "78210"
+//   },
+//   "thumbnail_image": {
+//     "S": "https://nftevening.com/wp-content/uploads/2022/05/Azuki-NFT-Founder.png"
+//   },
+//   "train_images_cnt": {
+//     "N": "2031"
+//   }
+// }
