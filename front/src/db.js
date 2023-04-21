d783e15b743a2f69fd411b46fc642b4b0233a68c
Shimhyerin <nier8702@kookmin.ac.kr>
2023-04-21 16:48:57 +0900

    feature : DB 관리  파일 public으로 변경

---
+++ front/src/db.js
@@ -0,0 +1,36 @@
+import AWS from 'aws-sdk';
+
+const {
+  REACT_APP_REGION,
+  REACT_APP_ACCESSKEY_ID,
+  REACT_APP_SECRETACCESSKEY,
+} = process.env;
+
+AWS.config.update({
+  region: REACT_APP_REGION,
+  accessKeyId: REACT_APP_ACCESSKEY_ID,
+  secretAccessKey: REACT_APP_SECRETACCESSKEY,
+});
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const dbName = 'TF_database';
+
+const params = {
+  TableName: dbName,
+};
+
+const main_case_params = {
+  TableName: dbName,
+  Limit: 6,
+  ScanIndexForward: false,
+  IndexName: 'result-index',
+  KeyConditionExpression: "#res = :result",
+  ExpressionAttributeNames: {
+    "#res": "result"
+  },
+  ExpressionAttributeValues: {
+    ":result": 'danger'
+  }
+}
+
+export {dynamoDB, params, main_case_params};
