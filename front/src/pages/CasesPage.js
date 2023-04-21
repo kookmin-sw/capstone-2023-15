8e5f83ef5c236fabae50e8b12b195c67e7651ded
Shimhyerin <nier8702@kookmin.ac.kr>
2023-04-21 16:54:21 +0900

    fix : data 캐싱 삭제

--- front/src/pages/CasesPage.js
+++ front/src/pages/CasesPage.js
@@ -6,32 +6,6 @@ import { dynamoDB, params } from '../db.js';
 const CasePage = () => {
 	const  [cases, setCases] = useState([]);
 
-	// 캐싱 O
-	// useEffect(() => {
-	// 	// 데이터를 가져올 때, 캐시된 데이터가 있으면 사용하도록 함
-	// 	const cachedData = localStorage.getItem('cachedData');
-	// 	if (cachedData) {
-	// 	  setCases(JSON.parse(cachedData));
-	// 	} else {
-	// 	  // 캐시된 데이터가 없으면, 서버에서 데이터를 가져옴
-	// 	  dynamoDB.scan(params, function(err, data) {
-	// 		if (err) {
-	// 		  console.log(err);
-	// 		} else {
-	// 		  const items = data.Items
-	// 		  .map(({ thumbnail_image, collection_name, malicious_images }) => ({
-	// 			thumbnail_image,
-	// 			collection_name,
-	// 			scam_length: Math.floor(malicious_images.slice(1, -1).split(',').length/2)
-	// 		  }))
-	// 		  setCases(items); 
-	// 		  localStorage.setItem('cachedData', JSON.stringify(items)); // 가져온 데이터를 로컬 스토리지에 저장
-	// 		}  
-	// 	  });
-	// 	}
-	// }, []);
-
-	// 캐싱 X 
 	useEffect(() => {
 		dynamoDB.scan(params, function(err, data) {
 		if (err) {
